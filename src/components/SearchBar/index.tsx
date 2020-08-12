ef08d4fb06a9cd8afa05b698911ea4c582d318b3
Michał <mjbartlakowski@gmail.com>
2020-08-12 21:57:41 +0200

    update search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,61 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchUsers } from 'store/gitUsers';
 
 type Props = {};
+let initDone: boolean = false;
 
 const SearchBar: React.FC<Props> = (props) => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [searchValid, setSearchValid] = useState<boolean>(true);
+  const dispatch = useDispatch();
 
   const setSearchFromUrl = () => {
     const initialParams = new URLSearchParams(location.search);
     if (initialParams.has('q')) {
-      setSearchValue(initialParams.get('q'))
+      setSearchValue(initialParams.get('q'));
     }
-  }
+    initDone = true;
+  };
 
   const updateUrlSearch = () => {
     const params = new URLSearchParams();
-    params.append("q", searchValue);
+    if(searchValue) {
+      params.append("q", searchValue);
+    }
     window.history.pushState(null, null, `${location.origin}${location.pathname}?${params.toString()}`);
-  }
+  };
 
   useEffect(() => {
     if (!searchValid && searchValue) {
       setSearchValid(true);
     }
-    updateUrlSearch();
-    if (!searchValue) {
+    if (!searchValue && !initDone) {
       setSearchFromUrl();
     }
-  })
+    if (initDone) {
+      updateUrlSearch();
+    }
+  }, [searchValue]);
 
-  const handleSearch = () => {
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
     if (!searchValue) {
       setSearchValid(false);
       return;
     }
-    console.log('search for user: ', searchValue);
-  }
+    dispatch(searchUsers(searchValue));
+  };
 
   return (
-    <div className="row">
-      <div className="col-xl-6 form-group">
-        <input
-          className={`form-control${ !searchValid ? ' is-invalid' : '' }`}
-          type="text"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          required
-        />
-        <div className="invalid-feedback">
-          Please enter username.
-        </div>
-        <div className="btn btn-primary text-center mt-2 w-100" onClick={() => handleSearch()}>
-          Search
+    <form onSubmit={handleSearch}>
+      <div className="row">
+        <div className="col-xl-6 form-group">
+          <input
+            className={`form-control${ !searchValid ? ' is-invalid' : '' }`}
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            required
+          />
+          <div className="invalid-feedback">
+            Please enter username.
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary text-center mt-2 w-100"
+          >
+            Search
+          </button>
         </div>
       </div>
-    </div>
-  )
-}
+    </form>
+  );
+};
 
 export default SearchBar;
